f768efaa9ad436ceb8ec9b4da977da0015f6a54a
nicola <notsecurity@gmail.com>
2014-09-26 16:40:01 -0700

    tasks are async tasks are async, fix #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var deploy = require('fxos-deploy/command');
+var reloadcss = require('fxos-reloadcss/command');
+
 module.exports = function (grunt) {
 
   grunt.initConfig({
@@ -25,22 +28,26 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('deployto', 'Deploy', function () {
-    var deploy = require('fxos-deploy/command');
+    var done = this.async();
     deploy({
       exit: true,
       manifestURL: './src/manifest.webapp',
       zip: './build/app.zip'
-    }, null, function () {
-      console.log('Deploy successful');
-    });
+    }, null, function (err) {
+      if (!err) console.log('Deploy successful');
+      done(err);
+    })
   });
 
   grunt.registerTask('reloadcss', 'Reload CSS', function () {
-    var reloadcss = require('fxos-reloadcss/command');
+    var done = this.async();
     reloadcss({
       manifestURL: './src/manifest.webapp',
       exit: true
-    }, null);
+    }, null, function(err) {
+      if (!err) console.log('Reload successful');
+      done(err);
+    });
   });
 
   grunt.registerTask('deploy', ['zip', 'deployto']);
